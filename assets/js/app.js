ba2bd7b17c3a8e84f5fa0eb6cfcaf2b278a845fa
Jackson Myhre <jack421myhre@gmail.com>
2022-10-08 17:25:39 -0500

    Targeted HTML elements through JS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-console.log('hello');
+
+const timerBtn = document.getElementById('timer-button');
+let minutes = document.getElementById('minutes').innerHTML;
+let seconds = document.getElementById('seconds').innerHTML;
+console.log(minutes, seconds);
+
+let minuteInterval = setInterval(() => {
+
+}, 60000);
+
+
+
+
+
+
+
+// ----------------------
+// --- Event Listener ---
+// ----------------------
+
+
+
+
+
